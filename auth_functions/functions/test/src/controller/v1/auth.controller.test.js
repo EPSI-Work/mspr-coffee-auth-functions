fd146cc7186d9b2a31d439ffa8669f8f921d26dc
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-03-15 17:37:25 +0100

    Update test config

--- auth_functions/functions/test/src/controller/v1/auth.controller.test.js
+++ auth_functions/functions/test/src/controller/v1/auth.controller.test.js
@@ -7,7 +7,7 @@ const { app } = require("functions/index.js");
 
 describe("V1SignInWithEmail", () => {
     it("should return a success message when the email parameter is given.", async() => {
-        const email = "test@test.com";
+        const email = "test@gmail.com";
         const response = await request(app)
             .post("/auth/v1/signInWithEmail")
             .send({ email: email });
@@ -28,9 +28,38 @@ describe("V1SignInWithEmail", () => {
         expect(response.status).toBe(400);
     }, 6000);
 
-    it("should return an error when the email given doesn't exist in the database.", async() => {
-        const response = await request(app).post("/auth/v1/signInWithEmail").send({
-            email: "test",
+    it("should return a success message when the email is send.", async() => {
+        const response = await request(app).post("/v1/signInWithEmail").send({
+            email: "test@gmail.com",
+        });
+        expect(response.status).toBe(200);
+    }, 6000);
+
+    //validate Token
+    it("should return an error message when the token is not given.", async() => {
+        const response = await request(app).get("/v1/validateQrCode");
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toContainEqual({
+            msg: "Token is required.",
+            param: "firebaseToken",
+            location: "query",
+        });
+    }, 6000);
+    it("should return an error message when before send a qrcode a token is given but not a good one.", async() => {
+        const response = await request(app).get(
+            "/v1/validateQrCode?firebaseToken=bonjour"
+        );
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toContainEqual({
+            msg: "Invalid Firebase token",
+            param: "firebaseToken",
+            location: "query",
+            value: "bonjour",
+        });
+    }, 6000);
+    it("should return an error message when before talk with rust a token is given but not a good one.", async() => {
+        const response = await request(app).post("/v1/verifyToken").send({
+            firebaseToken: "bonjour",
         });
         expect(response.status).toBe(400);
     }, 6000);
